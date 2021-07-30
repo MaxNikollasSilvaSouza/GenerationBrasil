db690b807f1f149ee6234354610470a1bce690b9
Max_Silva <maxnikollas@gmail.com>
2021-07-30 15:25:57 -0300

    Loja de Games Spring

--- Spring/Loja_de_Games/src/main/java/com/GameGeneration/Loja_de_Games/controller/CategoriaController.java
+++ Spring/Loja_de_Games/src/main/java/com/GameGeneration/Loja_de_Games/controller/CategoriaController.java
@@ -2,19 +2,27 @@ package com.GameGeneration.Loja_de_Games.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.GameGeneration.Loja_de_Games.model.Categoria;
 import com.GameGeneration.Loja_de_Games.repository.CategoriaRepository;
 
+
 @RestController
-@RequestMapping("/Games")
+@RequestMapping
 @CrossOrigin("*")
 public class CategoriaController {
 
@@ -35,20 +43,31 @@ public class CategoriaController {
 		}
 	}
 	
-	@GetMapping("/{Id}")
-	public ResponseEntity<Object> buscarPorId(@PathVariable(value = "id_categoria") Long id)
+	@GetMapping("/findbyid/{Id}")
+	public ResponseEntity<Categoria> buscarPorId(@PathVariable Long Id)
 	{
-		List<Categoria> registro = repositoriu.findById(id).get();
-		if(registro.isEmpty())
-		{
-			
-		}
-		else
-		{
-		return ResponseEntity.status(200).body(repositoriu.findById(id).get());
-		}
+		return repositoriu.findById(Id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+		//return ResponseEntity.status(200).body(repositoriu.findById(Id).get());
+	
 	}
 	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(repositoriu.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Categoria> atualizar(@Valid @RequestBody Categoria categoria)
+	{
+		return ResponseEntity.status(HttpStatus.OK).body(repositoriu.save(categoria));
+	}
+	
+	@DeleteMapping("/excluir/{Id}")
+	public void excluir(@PathVariable Long Id)
+	{
+		repositoriu.deleteById(Id);
+	}
 	
 	
 	
