cd9f8b88a67b7d01d0315c3cd9ede72a7f584837
Max_Silva <maxnikollas@gmail.com>
2021-08-02 11:42:55 -0300

    Blog Pessoal Completo

--- Blog_Pessoal/BlogPessoal/src/main/java/org/generation/BlogPessoal/model/Postagem.java
+++ Blog_Pessoal/BlogPessoal/src/main/java/org/generation/BlogPessoal/model/Postagem.java
@@ -4,11 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.Date;
 
@@ -30,6 +33,11 @@ public class Postagem {
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("Postagem")
+	private Tema tema;
+	
 	
 	public long getId() {
 		return id;
@@ -56,6 +64,12 @@ public class Postagem {
 		this.date = date;
 	}
 	
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 	
 	
 
